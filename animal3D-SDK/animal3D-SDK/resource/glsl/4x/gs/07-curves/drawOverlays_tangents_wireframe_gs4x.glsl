e7880599602038a1d241cb90db7017078138df91
Colton Soneson <colton.soneson@mymail.champlain.edu>
2020-02-25 12:10:32 -0500

    wireframe setup

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/gs/07-curves/drawOverlays_tangents_wireframe_gs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/gs/07-curves/drawOverlays_tangents_wireframe_gs4x.glsl
@@ -41,7 +41,39 @@
 //	6) draw tangent bases
 //	7) draw wireframe
 
+//1)
+//layout (points) in
+layout (triangles) in;
+
+//2) from vertex shader
+in vbVertexData {
+	mat4 vTangentBasis_view;
+	vec4 vTexcoord_atlas;
+	flat int vVertexID, vInstanceID, vModelID;
+} vVertexData[];	//3 bc triangle input
+
+//4)			second param is how many vertices output maximumm
+layout (line_strip, max_vertices = MAX_VERTICES) out;
+
+//5)
+out vec4 vColor;
+
+void drawWireFrame()
+{
+	vColor = vec4(1.0, 0.5, 0.0, 1.0);
+	gl_Position = gl_in[0].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[2].gl_Position;
+	EmitVertex();
+	gl_Position = gl_in[0].gl_Position;
+	EmitVertex();
+
+	EndPrimitive();
+}
+
 void main()
 {
-	
+	drawWireFrame();
 }
